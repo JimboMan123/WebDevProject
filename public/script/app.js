509ea24551359381a997cd59841b5939d2bffab1
Emil <emil.assarsson@hotmail.se>
2021-03-01 14:05:54 +0100

    divided script code

---
+++ public/script/app.js
@@ -0,0 +1,9 @@
+function openSlideMenuFunction() {
+    document.getElementById("menu").style.width = "300px";
+    document.getElementById("slideLogo").style.color = "#01c355";
+}
+
+function closeSlideMenuFunction() {
+    document.getElementById("menu").style.width = "0px";
+    document.getElementById("slideLogo").style.color = "#f3f1f1";
+}
