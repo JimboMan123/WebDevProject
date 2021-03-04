b36e70ebfbc3cefa9bf13cc24b52c7727e66a46a
Emil <emil.assarsson@hotmail.se>
2021-03-04 17:33:53 +0100

    shoppingcart script added, shopping cart ui added

--- public/script/app.js
+++
@@ -1,9 +0,0 @@
-function openSlideMenuFunction() {
-    document.getElementById("menu").style.width = "300px";
-    document.getElementById("slideLogo").style.color = "#01c355";
-}
-
-function closeSlideMenuFunction() {
-    document.getElementById("menu").style.width = "0px";
-    document.getElementById("slideLogo").style.color = "#f3f1f1";
-}
