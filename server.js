9948b62ea36b95aedea0da8592d2a56b25134542
JimboMan123 <tauriainenjimmie@live.se>
2021-03-07 17:47:38 +0100

    added ejs library and a login page
    added ejs for easy rendering of html pages.

--- server.js
+++ server.js
@@ -8,6 +8,13 @@ const PORT = 3000;
 
 app.use(express.static(path.join(__dirname, 'public')));
 
+app.set('view-engine', 'ejs');
+
+app.get('/login', (req, res) => {
+    res.render('login.ejs')
+  })
+
+
 
 app.listen(PORT, ()=> {
     console.log('App is listening at http://localhost:'+PORT)
