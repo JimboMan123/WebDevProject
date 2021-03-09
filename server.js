50f4b72e9269b2bd52ce85b2c51c643fd033d86c
JimboMan123 <tauriainenjimmie@live.se>
2021-03-09 13:59:21 +0100

    conversion done
    converted add html pages to ejs pages. Also added routes to the ejs pages.
    TODO: remove all html pages (pages with html extension)

--- server.js
+++ server.js
@@ -7,6 +7,34 @@ const PORT = 3000;
 
 app.set('view engine', 'ejs');
 
+app.get('/', function(request, response) {
+	
+  response.render('index.ejs');
+       
+response.end();
+});
+
+app.get('/index', function(request, response) {
+	
+  response.render('index.ejs');
+       
+response.end();
+});
+
+app.get('/cart', function(request, response) {
+	
+  response.render('cart.ejs');
+       
+response.end();
+});
+
+app.get('/phones', function(request, response) {
+	
+  response.render('phones.ejs');
+       
+response.end();
+});
+
 app.get('/login', function(request, response) {
 	
   response.render('login.ejs');
